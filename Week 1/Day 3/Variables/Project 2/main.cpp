642653b955e25b7655b3f2744bfe0514240e7c70
tkristof7 <toth.kristof.peter@gmail.com>
2019-04-03 15:13:32 +0200

    csere

---
+++ Week 1/Day 3/Variables/Project 2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+
+    int a = 526;
+    int b = 123;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+
+    return 0;
+}
