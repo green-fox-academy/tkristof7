13616ef2abed189243a29e97b2bc3430d957036d
tkristof7 <toth.kristof.peter@gmail.com>
2019-04-04 16:36:28 +0200

    Sum

--- Week 1/Day 4/Project 4/main.cpp
+++ Week 1/Day 4/Project 4/main.cpp
@@ -4,17 +4,20 @@
 int sum(int number);
 
 int main(int argc, char *args[]) {
-
-
+    int vari=6;
+    std::cout << sum(vari)/2<<std::endl;
     // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
 
     return 0;
 }
 
 int sum(int number) {
+    int sim =0;
+    for (int i = 0; i <= number; i++) {
 
-    for (int i = 0; i < number; i++) {
+        sim +=i;
 
-        sum = i
     }
+    return sim;
 }
+
