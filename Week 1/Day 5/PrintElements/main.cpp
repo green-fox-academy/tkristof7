a5533d063fb9e95b64d31819701cd4d46da3c4a3
tkristof7 <toth.kristof.peter@gmail.com>
2019-04-07 19:04:20 +0200

    Simple print

---
+++ Week 1/Day 5/PrintElements/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *args[]) {
+    int numbers[]{4, 5, 6, 7};
+
+    std::cout << numbers[0] << std::endl;
+    std::cout << numbers[1] << std::endl;
+    std::cout << numbers[2] << std::endl;
+    std::cout << numbers[3] << std::endl;
+
+
+    // - Create an array variable named `numbers`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print all the elements of `numbers`
+
+    return 0;
+}
