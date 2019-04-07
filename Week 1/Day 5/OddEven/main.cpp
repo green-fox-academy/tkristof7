5c1acdd7e563845d0d2a2b5c2bfd7accb24c007b
tkristof7 <toth.kristof.peter@gmail.com>
2019-04-07 12:13:59 +0200

    odd repair finish greet

--- Week 1/Day 5/OddEven/main.cpp
+++ Week 1/Day 5/OddEven/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
+#include <string>
 
+int main() {
+    std::cout << "Please enter the number: " << std::endl;
 
-int main(int argc, char *args[]) {
-    int number = 8;
-
-    if (number % 2 == 0) {
-        std::cout << "Even" << std::endl;
-    } else {
-        std::cout << "odd" << std::endl;
-    }
     // Write a program that reads a number from the standard input,
     // Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+
+    int a;
+
+    std::cin >> a;
+
+    if (a % 2 == 0)
+
+        std::cout << a << " is even" << std::endl;
+
+    else std::cout << a << " is odd" << std::endl;
+
 
     return 0;
 }
