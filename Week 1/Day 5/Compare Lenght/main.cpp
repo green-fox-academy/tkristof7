67c4cf1cdf9e1171782e8cdb85a0c1c28722beb8
tkristof7 <toth.kristof.peter@gmail.com>
2019-04-07 16:05:06 +0200

    count elements sizeof dont forget

---
+++ Week 1/Day 5/Compare Lenght/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    int p1[3] = {1, 2, 3};
+    int p2[2] = {4, 5};
+
+    if (sizeof(p1) > sizeof(p2)) {
+        std::cout << "p1 has more elements than p2. " << std::endl;
+    }
+
+    // - Create an array variable named `p1`
+    //   with the following content: `[1, 2, 3]`
+    // - Create an array variable named `p2`
+    //   with the following content: `[4, 5]`
+    // - Print if `p2` has more elements than `p1`
+
+    return 0;
+}
