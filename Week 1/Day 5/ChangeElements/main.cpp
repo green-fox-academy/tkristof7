7e4df679d0303890a2df6ccc8b8d10b5f5316652
tkristof7 <toth.kristof.peter@gmail.com>
2019-04-07 16:27:36 +0200

    array element changing

---
+++ Week 1/Day 5/ChangeElements/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *args[]) {
+    int s[6] = {1, 2, 3, 4, 5, 6};
+
+    std::cout << s[3] << std::endl;
+
+
+    // - Create an array variable named `s`
+    //   with the following content: `[1, 2, 3, 8, 5, 6]`
+    // - Change the 8 to 4
+    // - Print the fourth element
+
+    return 0;
+}
