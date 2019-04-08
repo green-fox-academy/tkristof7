d248ae3af629a5a53514e9e6574142882e932c52
tkristof7 <toth.kristof.peter@gmail.com>
2019-04-08 13:33:03 +0200

    print age value

---
+++ Week 2/Day 1/Print_Value/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Print out the value of the variable called 'age' using a pointer
+    // (you have to create the pointer first)
+
+    int age = 31;
+    int *agePointer = &age;
+    std::cout << *agePointer << std::endl;
+
+    return 0;
+}
