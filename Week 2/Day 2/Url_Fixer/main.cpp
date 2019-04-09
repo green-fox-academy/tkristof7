c963a8ecec02281894f28e25351c1fa55df58cd4
tkristof7 <toth.kristof.peter@gmail.com>
2019-04-09 13:25:01 +0200

    Simple Version

---
+++ Week 2/Day 2/Url_Fixer/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[])
+{
+    std::string url("https//www.reddit.com/r/nevertellmethebots");
+    url.replace(38, 4, "odds" );
+    url.insert(5, ":");
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crucial component, find out what it is and insert it too!
+
+    std::cout << url << std::endl;
+
+    return 0;
+}
