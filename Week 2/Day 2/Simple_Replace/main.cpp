d4a8e6b6fe3a26f2c7bf3aba05b8361b62b49535
tkristof7 <toth.kristof.peter@gmail.com>
2019-04-09 12:06:30 +0200

    cppreference replace

---
+++ Week 2/Day 2/Simple_Replace/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[])
+{
+    std::string example("In a dishwasher far far away");
+    //example.replace("dishwasher", "galaxy");
+    example.replace (5, 10, " galaxy ");
+
+    std::cout << example << std::endl;
+    // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+    // Please fix it for me!
+    // Expected output: In a galaxy far far away
+
+    return 0;
+}
